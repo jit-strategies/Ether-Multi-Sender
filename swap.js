54163972cace0470ef49d33ad66a8a48d0b29fcd
0xRomRom <roman0811@live.nl>
2023-04-22 20:36:06 +0200

    Transfer working

--- swap.js
+++ swap.js
@@ -13,7 +13,18 @@ const handler = async function (event, context) {
       "https://eth-goerli.g.alchemy.com/v2/L86T4ExZKmtSs0AWfbitM0mJ-YbNwzQn"
     );
 
-    const ACCOUNT_1 = new ethers.Wallet(env.WALLET_1, provider);
+    const privateKeyArray = [
+      process.env.WALLET_1,
+      process.env.W_2,
+      process.env.W_3,
+      process.env.W_4,
+      process.env.W_5,
+      // Add more private keys here
+    ];
+
+    const accountWallets = privateKeyArray.map(
+      (privateKey) => new ethers.Wallet(privateKey, provider)
+    );
 
     const V2_ROUTER = new ethers.Contract(
       process.env.V2_ROUTER_ADDRESS,
@@ -23,16 +34,20 @@ const handler = async function (event, context) {
     const PATH = [process.env.WETH_ADDRESS, process.env.TOKEN_ADDRESS];
     const DEADLINE = Math.floor(Date.now() / 1000) + 60 * 10; // 10 minutes
 
-    const transaction = await V2_ROUTER.connect(
-      ACCOUNT_1
-    ).swapExactETHForTokens(0, PATH, ACCOUNT_1.address, DEADLINE, {
-      value: swapAmount,
-    });
+    for (let i = 0; i < accountWallets.length; i++) {
+      const transaction = await V2_ROUTER.connect(
+        accountWallets[i]
+      ).swapExactETHForTokens(0, PATH, accountWallets[i].address, DEADLINE, {
+        value: swapAmount,
+      });
+
+      console.log(`Swap ${i} Succesful. Hash [${transaction.hash}]`);
+    }
 
     console.log(`Swap Complete!`);
-    console.log(transaction.hash);
+
     console.log(
-      `See transaction at: https://sepolia.etherscan.io/tx/${transaction.hash}/`
+      `See transactions at: https://goerli.etherscan.io/address/${accountWallets[0].address}`
     );
   } catch (err) {
     console.error(err);
--- transfer.js
+++ transfer.js
@@ -2,43 +2,48 @@ const { ethers } = require("ethers");
 require("dotenv").config();
 const IERC20 = require("@openzeppelin/contracts/build/contracts/ERC20.json");
 
-//Current send amount: 5 USDC
-const transferAmount = 5000000;
+//Current send amount: 50 USDC
+const transferAmount = 50000000;
 
 const handler = async function () {
-  console.log("== Starting transaction ==");
+  try {
+    console.log("== Starting transaction ==");
 
-  //Change to the right network provider (Currently Goerli Testnet)
-  const provider = new ethers.providers.JsonRpcProvider(
-    "https://eth-goerli.g.alchemy.com/v2/L86T4ExZKmtSs0AWfbitM0mJ-YbNwzQn"
-  );
-
-  const privateKeyArray = [
-    process.env.WALLET_1,
-    process.env.W_2,
-    process.env.W_3,
-    process.env.W_4,
-    process.env.W_5,
-    // Add more private keys here
-  ];
-
-  const accountWallets = privateKeyArray.map(
-    (privateKey) => new ethers.Wallet(privateKey, provider)
-  );
-
-  const ERC20 = new ethers.Contract(env.TOKEN_ADDRESS, IERC20.abi);
-
-  for (let i = 1; i < accountWallets.length; i++) {
-    const transaction = await ERC20.connect(accountWallets[0]).transfer(
-      accountWallets[i].address,
-      transferAmount
+    //Change to the right network provider (Currently Goerli Testnet)
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://eth-goerli.g.alchemy.com/v2/L86T4ExZKmtSs0AWfbitM0mJ-YbNwzQn"
     );
-    console.log(`Transfer ${i} Succesful. Hash#${transaction.hash}#`);
-  }
 
-  console.log(`== Transfers Complete! ==`);
-  console.log(
-    `See transactions at: https://goerli.etherscan.io/address/${accountWallets[0].address}`
-  );
+    const privateKeyArray = [
+      process.env.WALLET_1,
+      process.env.W_2,
+      process.env.W_3,
+      process.env.W_4,
+      process.env.W_5,
+      // Add more private keys here
+    ];
+
+    const accountWallets = privateKeyArray.map(
+      (privateKey) => new ethers.Wallet(privateKey, provider)
+    );
+
+    const ERC20 = new ethers.Contract(process.env.TOKEN_ADDRESS, IERC20.abi);
+
+    for (let i = 1; i < accountWallets.length; i++) {
+      const transaction = await ERC20.connect(accountWallets[0]).transfer(
+        accountWallets[i].address,
+        transferAmount
+      );
+      console.log(`Transfer ${i} Succesful. Hash [${transaction.hash}]`);
+    }
+
+    console.log(`== Transfers Complete! ==`);
+    console.log(
+      `See transactions at: https://goerli.etherscan.io/address/${accountWallets[0].address}`
+    );
+  } catch (err) {
+    console.error(err);
+  }
 };
+
 handler();
