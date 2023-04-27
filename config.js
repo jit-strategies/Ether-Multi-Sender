45e37aad25daa6f1fdbf0020fb20bb80b3cc453d
0xRomRom <roman0811@live.nl>
2023-04-27 15:32:39 +0200

    File descriptions updated

--- config.js
+++ config.js
@@ -1,11 +1,19 @@
+// Network prover (Goerli in this example)
 exports.RPC =
   "https://eth-goerli.g.alchemy.com/v2/L86T4ExZKmtSs0AWfbitM0mJ-YbNwzQn";
+
+// ETH amount to send
 exports.ETH = "0.01";
 
+// Network specific Etherscan link for console logging purpose
+exports.ETHERSCAN = "https://goerli.etherscan.io/address";
+
+// Object of private keys, the first entry being the main sender's key
+// Add more keys if desired
 exports.keys = {
-  main: "6532a61bf28b1d08afe5bfd104ca259ce487c69405c5273026735d5c07c73ee6",
-  W_2: "192c66f7281e0f4c5e3484a07766d9aeb0ee7abc4d49bbae5cafdcecc1cb4f8d",
-  W_3: "7f6fbadc09106e7a6634da4133272a4016960e5070db9d56d54c60a076466a6b",
-  W_4: "e4ddf642b3a7c7da713fbc2cff7bf74078752a62915a9f0380b408abead3bfdf",
-  W_5: "6b1e18e952b495bbf6e0a6f55d24e5e661da4d28ffe3e95d5a93094f6af04dde",
+  main: "6532a61bf28b1d08afe5bfd104ca259ce487c69405c6273026735d5c07c83ee6",
+  W_2: "192c66f7281e0f4c5e3484a07766d9aeb0ee7abc4d49ccae5cafdcecc1ce4f8d",
+  W_3: "7f6fbadc09106e7a6634da4133272a4016960e5070db9d56e54c60a079466a6b",
+  W_4: "e4ddf642b3a7c7da713fbc2cff7bf74078752a62916f9f0380b408abead3bfdf",
+  W_5: "6b1e18e952b495bbf6e0a6f55d24e5e661da4d28ffe3e95d5a93092f6af04dde",
 };
--- transfer.js
+++ transfer.js
@@ -1,37 +1,51 @@
+// Bridge to access web3
 const { ethers } = require("ethers");
-require("dotenv").config();
 
-const { RPC, ETH } = require("./config"); // Import V2_ROUTER and RPC
-const { keys } = require("./config"); // Import keys as a separate object
+// Network, transfer amount, Network's etherscan
+const { RPC, ETH, ETHERSCAN } = require("./config");
 
+// Private key object
+const { keys } = require("./config");
+
+// Connect to blockchain
 const provider = new ethers.providers.JsonRpcProvider(RPC);
 
+// Store all keys in an array
 const keyArray = Object.entries(keys).map(([key, value]) => {
   return value;
 });
 
+// Create a wallet instance for every key
 const accountWallets = keyArray.map(
   (privateKey) => new ethers.Wallet(privateKey, provider)
 );
 
+// Amount to transfer
 const transferAmount = ethers.utils.parseEther(ETH);
 
+// Transfer function§
 (async function () {
   try {
     console.log("== Starting Transaction ==");
 
+    // Send ETH to every wallet in the array except for the sender
     for (let i = 1; i < accountWallets.length; i++) {
+      // Create transaction
       const transaction = await accountWallets[0].sendTransaction({
+        // For every wallet
         to: accountWallets[i].address,
+        // And send specified amount
         value: transferAmount,
       });
-      console.log(`Transfer ${i} Successful. Hash [${transaction.hash}]`);
+      console.log(`Transfer ${i} Successful. Tx => [${transaction.hash}]`);
     }
 
     console.log(`== Transfers Complete! ==`);
     console.log(
-      `See transactions at: https://goerli.etherscan.io/address/${accountWallets[0].address}`
+      `See transactions at: ${ETHERSCAN}/${accountWallets[0].address}`
     );
+
+    //Log errors to the terminal
   } catch (err) {
     console.error(err);
   }
